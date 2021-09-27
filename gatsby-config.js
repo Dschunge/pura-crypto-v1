a1e3ed60dcd3b4632ce6f8ab74996be19c06fa7f
Juergen Schaar <schaar.juergen@gmail.com>
2021-09-27 21:52:07 +0200

    Version changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Pura Crypto V1`,
+    title: `Pura Crypto V2`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
